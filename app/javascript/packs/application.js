2b2a82a2730e492f81c3cf9f219841cd2cfb525f
Cataline Baert <snuf_kin@yahoo.com>
2020-05-27 20:29:28 +0200

    implement modal to create meme

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,4 +31,31 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var memes = document.querySelectorAll(".meme");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  memes.forEach((meme) => {
+    meme.onclick = function () {
+      modal.style.display = "block";
+    }
+  })
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 });
